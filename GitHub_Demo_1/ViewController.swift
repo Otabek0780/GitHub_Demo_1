f5bc6f02a1c24515a5cee1bb0b4bcdd74cf37fe6
Otabek0780 <mrdunacdtm@gmail.com>
2021-01-27 21:36:57 +0900

    Initial Commit

---
+++ GitHub_Demo_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub_Demo_1
+//
+//  Created by kim jong moon on 27/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
