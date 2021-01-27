5dc7497fc76665862068105130b6f01d3093ff54
Otabek0780 <mrdunacdtm@gmail.com>
2021-01-27 22:08:37 +0900

    Welcome to Storyboard

---
+++ GitHub_Demo_1/Controllers/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  GitHub_Demo_1
+//
+//  Created by kim jong moon on 27/01/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet var homeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        homeLabel.text = "Welcome to Storyboard"
+
+    }
+
+
+
+}
